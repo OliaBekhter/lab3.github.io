e4799e1bbfd108700cd82503cc0e0eb6403e1763
OliaBekhter <olia.bekhter@gmail.com>
2021-11-22 08:22:05 +0200

    main

--- js/script.js
+++ js/script.js
@@ -3,14 +3,10 @@ function printSteps(){
 	let endVal = parseFloat(document.getElementById("end").value);
 	let stepVal = parseFloat(document.getElementById("step").value);
 	let y = 0;
-	if(isNaN(beginningVal && endVal && stepVal)){
-		console.log("Input number wrong!");
-		return false;
-	}
 	console.log("while:");
 	while(beginningVal <= endVal){
 		y = Math.sqrt(Math.pow(Math.sin(beginningVal), 2)+1/2);
-		console.log("x = " + (beginningVal) + " | y = " + (y));
+		console.log("x = " + (beginningVal) + ", y = " + (y));
 		beginningVal+=stepVal;
 	}
 	console.log("do-while:");
@@ -18,7 +14,7 @@ function printSteps(){
 	beginningVal = parseFloat(document.getElementById("beggining").value);
 	do{
 	  y = Math.sqrt(Math.pow(Math.sin(beginningVal), 2)+1/2);
-	  console.log("x = " + (beginningVal) + " | y = " + (y));
+	  console.log("x = " + (beginningVal) + ", y = " + (y));
 	  beginningVal+=stepVal;
 	}
 	while (beginningVal <= endVal)
@@ -26,24 +22,22 @@ function printSteps(){
 	y = 0;
 	for (beginningVal = parseFloat(document.getElementById("beggining").value); beginningVal <= endVal; beginningVal+=stepVal) {
 	  y = Math.sqrt(Math.pow(Math.sin(beginningVal), 2)+1/2);
-	  console.log("x = " + (beginningVal) + " | y = " + (y));
+	  console.log("x = " + (beginningVal) + ", y = " + (y));
 	}
-	return false;
 }
-   
+ 
 function draw() {
 	let canvas = document.getElementById('canvas');
 	let ctx = canvas.getContext('2d');
 	canvas.width = 500;
 	canvas.height = 500;
-	ctx.fillStyle = "antiquewhite";
 	ctx.beginPath();
 	console.log("canvas (in 500X zoom):");
 	let y = 0;
 	for (let x = 0; x <= 0.2; x+=0.025) {
 		y = Math.sqrt(Math.pow(Math.sin(x), 2)+1/2);
 		ctx.lineTo( x*500,  y*500);
-		console.log("x = " + (x*500) + " | y = " + (y*500));
+		console.log("x = " + (x*500) + ", y = " + (y*500));
 	}
 	ctx.stroke();
 }
